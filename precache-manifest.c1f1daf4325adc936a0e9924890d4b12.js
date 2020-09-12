69d6458b3ff9243836bf72d2cf68ee5089e4f2c0
Mark Gire <magire01@gmail.com>
2020-09-12 03:40:41 -0400

    Updates

---
+++ precache-manifest.c1f1daf4325adc936a0e9924890d4b12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c47a8c96c09b673da25a21ada60dd27",
+    "url": "/ar-poetry-react/index.html"
+  },
+  {
+    "revision": "0842e053194a30689af6",
+    "url": "/ar-poetry-react/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "6f878092b95c62776721",
+    "url": "/ar-poetry-react/static/css/main.24da1084.chunk.css"
+  },
+  {
+    "revision": "0842e053194a30689af6",
+    "url": "/ar-poetry-react/static/js/2.6e67f423.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ar-poetry-react/static/js/2.6e67f423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f878092b95c62776721",
+    "url": "/ar-poetry-react/static/js/main.011997d4.chunk.js"
+  },
+  {
+    "revision": "7351e1c78c1fb73d7478",
+    "url": "/ar-poetry-react/static/js/runtime-main.926be8c0.js"
+  },
+  {
+    "revision": "a8801c185fded8a221f6c62feb6694d4",
+    "url": "/ar-poetry-react/static/media/backgroundImage2.a8801c18.jpg"
+  }
+]);
