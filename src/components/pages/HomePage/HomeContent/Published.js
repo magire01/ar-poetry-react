4b3223d16f681949ac569dc66f6f9fc1a8023760
Mark Gire <markgire@Marks-MacBook-Pro.local>
2020-09-19 17:34:22 -0400

    Edited carousel to remove warning

--- src/components/pages/HomePage/HomeContent/Published.js
+++ src/components/pages/HomePage/HomeContent/Published.js
@@ -1,14 +1,47 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core/";
+import { Grid, Typography, Card, CardHeader, CardContent } from "@material-ui/core/";
 
 const PublishedSection = () => {
 
     return (
 
         <Grid container style={{ marginTop: "5%" }}>
-            <Typography>
-                This is the Published Section
-            </Typography>
+            <Grid item md="12" xs="12" style={{ width: 100, height:"auto", textAlign: "center"}}>
+                <Typography variant="h6">Published</Typography>
+            </Grid>
+            <Grid item md="12" xs="12">
+                <Typography style={{ paddingRight: 40, paddingLeft: 10, marginTop: "2%", textAlign: "center" }}>
+                    I have been published at the following places:
+                    Something Poetry
+                    Something Else Inc.
+                </Typography>
+            </Grid>
+            <Grid item md="6" xs="12" style={{ width: 100, height:"auto", textAlign: "center"}}>
+                <Card>
+                    <CardContent>
+                        <Typography variant="subtitle1">Poem 1</Typography>
+                        <Typography>
+                            Test Test Poem Test Poem Test 
+                            Poem Test Poem Test Poem Test 
+                            Poem Test Poem Test 
+                            Poem Test Poem Test 
+                        </Typography>    
+                    </CardContent>   
+                </Card>
+            </Grid>
+            <Grid item md="6" xs="12" style={{ width: 100, height:"auto", textAlign: "center"}}>
+                <Card>
+                    <CardContent>
+                        <Typography variant="subtitle1">Poem 2</Typography>
+                        <Typography>
+                            Test Test Poem Test Poem Test 
+                            Poem Test Poem Test Poem Test 
+                            Poem Test Poem Test 
+                            Poem Test Poem Test 
+                        </Typography>    
+                    </CardContent>   
+                </Card>
+            </Grid>
         </Grid>
     )
 }
